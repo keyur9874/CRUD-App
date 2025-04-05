dff7c478165b80027ebf0dce82c44635adb107ef
keyur.asodariya <keyur.asodariya@veloxcore.com>
2025-04-05 17:06:50 +0530

    Added model entity

---
+++ CRUD-API/CRUD-API/Models/User.cs
@@ -0,0 +1,33 @@
+﻿namespace CRUD_API.Models
+{
+    /// <summary>
+    /// Represents a user within the system.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier of the user.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name of the user.
+        /// </summary>
+        public required string FirstName { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the last name of the user.
+        /// </summary>
+        public required string LastName { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the email address of the user. This field is optional.
+        /// </summary>
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the user is marked as deleted.
+        /// </summary>
+        public bool IsDeleted { get; set; } = false;
+    }
+}
